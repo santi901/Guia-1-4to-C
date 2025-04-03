50e896bfeffcb86a128b4e606e8fd9dc882cf646
santi901 <49379041@est.ort.edu.ar>
2025-04-03 12:10:41 -0300

    guia 2 empezada

---
+++ Assets/Scripts/SumayProducto.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SumayProducto : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
