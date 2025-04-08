b87b2e0fb4f49bd8a4acd0ab5d9210a91a6eb0ce
santi901 <49379041@est.ort.edu.ar>
2025-04-08 09:05:10 -0300

    Tema 1 evaluacion

---
+++ Assets/Scripts/TemaUno.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemaUno : MonoBehaviour
+{
+    public float Dinero;
+    public string Moneda;
+
+
+    float MonedaConvertida;
+    float MontoMinimo = 1000;
+    float CotisacionMonedaElegida;
+    float Dolar = 1345f;
+    float Euro = 936.2f;
+    float Real = 173.3f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Dinero < MontoMinimo)
+        {
+            Debug.Log("El monto es menor a 1000");
+            return;
+        }
+        if (Moneda == "D")
+        {
+            CotisacionMonedaElegida = Dolar;
+        }
+        else if (Moneda == "E")
+        {
+            CotisacionMonedaElegida = Euro;
+        }
+        else if (Moneda == "R")
+        {
+            CotisacionMonedaElegida = Real;
+        }
+        else
+        {
+            Debug.Log("Opción de moneda extranjera no válida");
+            return;
+        }
+        
+
+        Dinero = Dinero / CotisacionMonedaElegida;
+
+        Debug.Log(Dinero);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
